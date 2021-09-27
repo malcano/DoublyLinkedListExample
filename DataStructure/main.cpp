f24838e1b71a101b0c9eaf75da8894ca36be98cc
malcano <54932717+malcano@users.noreply.github.com>
2021-09-27 15:21:36 +0900

    Initial Commit

---
+++ DataStructure/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  DataStructure
+//
+//  Created by Minho Lee on 2021/09/27.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
